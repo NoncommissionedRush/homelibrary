95407701b3447936c511e6fecadcc37f8351fbb6
NoncommissionedRush <milosavljevic.viktor@gmail.com>
2022-02-07 13:36:42 +0100

    update dirname

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ import session from "express-session";
 import { sessionConfig } from "./config.js";
 import "dotenv/config";
 import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 5000;
 
