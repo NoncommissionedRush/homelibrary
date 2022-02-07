185f813007a5b88fc25700a5f3daae6dc3d4cc97
NoncommissionedRush <milosavljevic.viktor@gmail.com>
2022-02-07 10:02:11 +0100

    add build path to app.js

--- app.js
+++ app.js
@@ -8,11 +8,20 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.use(session(sessionConfig));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", router);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
