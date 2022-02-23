dd105cbd107e6ecbf0131f3f0e77d334c733d98d
NoncommissionedRush <milosavljevic.viktor@gmail.com>
2022-02-23 15:38:11 +0100

    add routes to add and remove tags

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { createBook, deleteBook, getBooks, updateBook } from "../queries.js";
+import getBooks from "../queries/getBooks.js";
+import createBook from "../queries/createBook.js";
+import updateBook from "../queries/updateBook.js";
+import deleteBook from "../queries/deleteBook.js";
+import { addTagToBookRequestHandler } from "../queries/addTagToBook.js";
+import { removeTagFromBookRequestHandler } from "../queries/removeTagFromBook.js";
 import { login, logout, me, register } from "../users.js";
 const router = express.Router();
 
@@ -35,4 +40,29 @@ router.get("/logout", logout);
 //@desc return current user object
 router.get("/me", me);
 
+//@route  POST /book_tag/:bookId
+//@desc add tag to book
+router.post("/book_tag/:bookId", addTagToBookRequestHandler);
+
+//@route  DELETE /book_tag/:bookId
+//@desc add tag to book
+router.delete("/book_tag/:bookId", removeTagFromBookRequestHandler);
+
 export default router;
+
+// sql query to get books with tags
+// SELECT book.id, book.title, array_agg(tag.tag)
+// FROM book
+// FULL JOIN book_tag bt
+// ON bt.book_id = book.id
+// FULL JOIN tag
+// ON tag.id = bt.tag_id
+// GROUP BY book.id;
+
+// sql query to delete orphans from tag table
+// DELETE FROM tag
+// USING tag as t
+// LEFT JOIN book_tag bt
+// ON bt.tag_id = t.id
+// WHERE tag.id = t.id
+// AND bt.tag_id IS NULL;
