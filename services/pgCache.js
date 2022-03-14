8bd6277b59e00427339c1293857cddb15f988ead
NoncommissionedRush <milosavljevic.viktor@gmail.com>
2022-03-14 23:02:21 +0100

    add caching middleware

---
+++ services/pgCache.js
@@ -0,0 +1,47 @@
+import pool from "../config.js";
+import redis from "redis";
+
+const client = redis.createClient();
+client.connect();
+
+/**
+ * Runs a Postgres query and stores the result in redis cache
+ *
+ * @param   {String}    query       SQL query
+ * @param   {String[]}  params      array of query parameters
+ * @param   {Integer}   expiration  cache expiration in seconds
+ * @returns {Object}    Array of objects returned from db / cache
+ */
+
+const Query = async (query, params, expiration) => {
+  let cache = true;
+
+  if (typeof params === "number") {
+    expiration = params;
+    params = [];
+  }
+
+  if (typeof expiration !== "number") {
+    cache = false;
+    next = expiration;
+  }
+
+  if (!cache) {
+    const response = await pool.query(query, params);
+    return response.rows;
+  }
+
+  const key = "postgres:" + query + JSON.stringify(params);
+
+  const cacheResult = await client.get(key);
+
+  if (cacheResult) return JSON.parse(cacheResult);
+
+  const response = await pool.query(query, params);
+
+  await client.set(key, JSON.stringify(response.rows), "EX", expiration);
+
+  return response.rows;
+};
+
+export default Query;
