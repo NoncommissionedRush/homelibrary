a29d95c733a29b0147c5281512fa71925eddf1cc
NoncommissionedRush <milosavljevic.viktor@gmail.com>
2022-03-17 13:47:18 +0100

    fix status send in auth middleware

--- services/authMiddleware.js
+++ services/authMiddleware.js
@@ -4,17 +4,18 @@ import "dotenv/config";
 const authMiddleware = (req, res, next) => {
   const token = req.header("x-auth-token");
 
-  if (!token) {
-    return res.sendStatus(401).send("No token, authorization denied");
+  if (token === "null") {
+    return res.status(401).send("No token, authorization denied");
   }
 
   try {
     const { user } = jwt.verify(token, process.env.JWT_SECRET);
+
     res.locals.user = user;
     next();
   } catch (error) {
     console.log(error);
-    return res.sendStatus(401).send("Token is not valid");
+    return res.status(401).send("Token is not valid");
   }
 };
 
