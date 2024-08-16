398d44c28713699bc4bed2514cdd60137645d722
Viktor Milosavljevič <viktor.milosavljevic@ringier.sk>
2024-08-16 12:12:13 +0200

    use api prefix for router

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import express from "express";
-import router from "./routes/router.js";
-import * as path from "path";
-import { fileURLToPath } from "url";
+import express from 'express';
+import router from './routes/router.js';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,17 +9,17 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", router);
+app.use('/api', router);
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 export default app;
