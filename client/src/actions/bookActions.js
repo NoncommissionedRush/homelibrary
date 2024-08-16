92cc8108a5ee69c8c76160dea8ef1bda4c6b3a5a
Viktor Milosavljevič <viktor.milosavljevic@ringier.sk>
2024-08-16 12:20:31 +0200

    prefix api requests

--- client/src/actions/bookActions.js
+++ client/src/actions/bookActions.js
@@ -1,155 +1,159 @@
-import axios from "axios";
+import axios from 'axios';
 import {
-  AXIOS_REQUEST_CONFIG,
-  ADD_BOOK,
-  ADD_TAG,
-  DELETE_BOOK,
-  GET_BOOKS,
-  SET_FILTER,
-  DELETE_TAG,
-  EDIT_BOOK,
-} from "./types";
+    AXIOS_REQUEST_CONFIG,
+    ADD_BOOK,
+    ADD_TAG,
+    DELETE_BOOK,
+    GET_BOOKS,
+    SET_FILTER,
+    DELETE_TAG,
+    EDIT_BOOK,
+} from './types';
 
-export const getBooks = () => async (dispatch) => {
-  const response = await axios.get("/books");
-  if (response) {
-    dispatch({
-      type: GET_BOOKS,
-      payload: response.data,
-    });
-    return true;
-  }
-  return false;
+export const getBooks = () => async dispatch => {
+    const response = await axios.get('/api/books');
+    if (response) {
+        dispatch({
+            type: GET_BOOKS,
+            payload: response.data,
+        });
+        return true;
+    }
+    return false;
 };
 
-export const addBook = (formData) => async (dispatch) => {
-  try {
-    const newBookId = await axios.post("/book", formData, AXIOS_REQUEST_CONFIG);
+export const addBook = formData => async dispatch => {
+    try {
+        const newBookId = await axios.post(
+            '/api/book',
+            formData,
+            AXIOS_REQUEST_CONFIG,
+        );
 
-    if (newBookId.data.errorMessage) {
-      return false;
+        if (newBookId.data.errorMessage) {
+            return false;
+        }
+
+        const newBook = await axios.get(`/api/book/${newBookId.data}`);
+
+        dispatch({
+            type: ADD_BOOK,
+            payload: newBook.data,
+        });
+
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+};
+
+export const deleteBook = id => async dispatch => {
+    const deletionSuccessful = await axios.delete(`/api/book/${id}`);
+
+    if (!deletionSuccessful) return;
+
+    dispatch({
+        type: DELETE_BOOK,
+        payload: id,
+    });
+};
+
+export const setFilter = filter => dispatch => {
+    dispatch({
+        type: SET_FILTER,
+        payload: filter,
+    });
+};
+
+export const deleteTag = (bookId, tag) => async dispatch => {
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    const formData = {
+        tagName: tag,
+    };
+
+    try {
+        await axios.delete(`/api/book_tag/${bookId}`, {
+            headers: headers,
+            data: formData,
+        });
+
+        const updatedBook = await axios.get(`/api/book/${bookId}`);
+
+        dispatch({
+            type: DELETE_TAG,
+            payload: updatedBook.data,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const addTag = (bookId, tag) => async dispatch => {
+    try {
+        const result = await axios.post(
+            `/api/book_tag/${bookId}`,
+            { tagName: tag },
+            AXIOS_REQUEST_CONFIG,
+        );
+
+        if (!result) return false;
+
+        const updatedBook = await axios.get(`/api/book/${bookId}`);
+
+        if (!updatedBook.data) return false;
+
+        dispatch({
+            type: ADD_TAG,
+            payload: updatedBook.data,
+        });
+
+        return true;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const updateBook = (bookId, editFormData) => async dispatch => {
+    let read;
+
+    if (editFormData['checkbox-bedo'] && editFormData['checkbox-zuzka']) {
+        read = 3;
+    } else if (editFormData['checkbox-zuzka']) {
+        read = 2;
+    } else if (editFormData['checkbox-bedo']) {
+        read = 1;
+    } else {
+        read = 0;
     }
 
-    const newBook = await axios.get(`/book/${newBookId.data}`);
+    const data = {
+        title: editFormData.title,
+        author: editFormData.author,
+        note: editFormData.note,
+        read: read,
+    };
 
-    dispatch({
-      type: ADD_BOOK,
-      payload: newBook.data,
-    });
+    try {
+        const updatedBookId = await axios.put(
+            `/api/book/${bookId}`,
+            data,
+            AXIOS_REQUEST_CONFIG,
+        );
 
-    return true;
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
-};
+        const updatedBook = await axios.get(`/api/book/${updatedBookId.data}`);
 
-export const deleteBook = (id) => async (dispatch) => {
-  const deletionSuccessful = await axios.delete(`/book/${id}`);
+        if (!updatedBook.data) return false;
 
-  if (!deletionSuccessful) return;
-
-  dispatch({
-    type: DELETE_BOOK,
-    payload: id,
-  });
-};
-
-export const setFilter = (filter) => (dispatch) => {
-  dispatch({
-    type: SET_FILTER,
-    payload: filter,
-  });
-};
-
-export const deleteTag = (bookId, tag) => async (dispatch) => {
-  const headers = {
-    "Content-Type": "application/json",
-  };
-
-  const formData = {
-    tagName: tag,
-  };
-
-  try {
-    await axios.delete(`/book_tag/${bookId}`, {
-      headers: headers,
-      data: formData,
-    });
-
-    const updatedBook = await axios.get(`/book/${bookId}`);
-
-    dispatch({
-      type: DELETE_TAG,
-      payload: updatedBook.data,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const addTag = (bookId, tag) => async (dispatch) => {
-  try {
-    const result = await axios.post(
-      `/book_tag/${bookId}`,
-      { tagName: tag },
-      AXIOS_REQUEST_CONFIG
-    );
-
-    if (!result) return false;
-
-    const updatedBook = await axios.get(`/book/${bookId}`);
-
-    if (!updatedBook.data) return false;
-
-    dispatch({
-      type: ADD_TAG,
-      payload: updatedBook.data,
-    });
-
-    return true;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const updateBook = (bookId, editFormData) => async (dispatch) => {
-  let read;
-
-  if (editFormData["checkbox-bedo"] && editFormData["checkbox-zuzka"]) {
-    read = 3;
-  } else if (editFormData["checkbox-zuzka"]) {
-    read = 2;
-  } else if (editFormData["checkbox-bedo"]) {
-    read = 1;
-  } else {
-    read = 0;
-  }
-
-  const data = {
-    title: editFormData.title,
-    author: editFormData.author,
-    note: editFormData.note,
-    read: read,
-  };
-
-  try {
-    const updatedBookId = await axios.put(
-      `/book/${bookId}`,
-      data,
-      AXIOS_REQUEST_CONFIG
-    );
-
-    const updatedBook = await axios.get(`/book/${updatedBookId.data}`);
-
-    if (!updatedBook.data) return false;
-
-    dispatch({
-      type: EDIT_BOOK,
-      payload: updatedBook.data,
-    });
-    return true;
-  } catch (error) {
-    console.log(error);
-  }
+        dispatch({
+            type: EDIT_BOOK,
+            payload: updatedBook.data,
+        });
+        return true;
+    } catch (error) {
+        console.log(error);
+    }
 };
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,90 +1,90 @@
-import axios from "axios";
-import React, { useState } from "react";
-import Alert from "react-bootstrap/Alert";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import axios from 'axios';
+import React, { useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function LoginForm({ setIsLoggedIn, toggleDisplay }) {
-  const [showError, setShowError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [formData, setFormData] = useState({
-    name: "",
-    password: "",
-  });
+    const [showError, setShowError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [formData, setFormData] = useState({
+        name: '',
+        password: '',
+    });
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
-  };
-
-  const logIn = async (e, formData) => {
-    e.preventDefault();
-
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-      },
+    const handleChange = e => {
+        setFormData({ ...formData, [e.target.id]: e.target.value });
     };
 
-    const response = await axios.post("/login", formData, config);
+    const logIn = async (e, formData) => {
+        e.preventDefault();
 
-    if (response.data.errorMessage) {
-      setShowError(true);
-      setErrorMessage(response.data.errorMessage);
-      return;
-    }
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
 
-    if (response.data) {
-      localStorage.setItem("jwt", response.data.token);
-      setIsLoggedIn(true);
-      toggleDisplay(false);
-    }
-  };
+        const response = await axios.post('/api/login', formData, config);
 
-  return (
-    <Form
-      style={{
-        width: "50%",
-        position: "fixed",
-        top: "40%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        zIndex: "6",
-        backgroundColor: "#fff",
-        padding: "30px",
-        display: "flex",
-        flexDirection: "column",
-      }}
-      id="login-form"
-    >
-      <Form.Group className="mb-3" controlId="name">
-        <Form.Label>Meno</Form.Label>
-        <Form.Control
-          type="text"
-          value={formData.name}
-          onChange={handleChange}
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="password">
-        <Form.Label>Heslo</Form.Label>
-        <Form.Control
-          type="password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-      </Form.Group>
-      {showError && <Alert variant="danger">{errorMessage}</Alert>}
-      <Button
-        style={{ alignSelf: "center" }}
-        variant="primary"
-        onClick={(e) => {
-          logIn(e, formData);
-          setFormData({ name: "", password: "" });
-        }}
-      >
-        Prihlásiť
-      </Button>
-    </Form>
-  );
+        if (response.data.errorMessage) {
+            setShowError(true);
+            setErrorMessage(response.data.errorMessage);
+            return;
+        }
+
+        if (response.data) {
+            localStorage.setItem('jwt', response.data.token);
+            setIsLoggedIn(true);
+            toggleDisplay(false);
+        }
+    };
+
+    return (
+        <Form
+            style={{
+                width: '50%',
+                position: 'fixed',
+                top: '40%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                zIndex: '6',
+                backgroundColor: '#fff',
+                padding: '30px',
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+            id="login-form"
+        >
+            <Form.Group className="mb-3" controlId="name">
+                <Form.Label>Meno</Form.Label>
+                <Form.Control
+                    type="text"
+                    value={formData.name}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="password">
+                <Form.Label>Heslo</Form.Label>
+                <Form.Control
+                    type="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            {showError && <Alert variant="danger">{errorMessage}</Alert>}
+            <Button
+                style={{ alignSelf: 'center' }}
+                variant="primary"
+                onClick={e => {
+                    logIn(e, formData);
+                    setFormData({ name: '', password: '' });
+                }}
+            >
+                Prihlásiť
+            </Button>
+        </Form>
+    );
 }
 
 export default LoginForm;
